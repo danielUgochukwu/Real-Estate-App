bda8e347344e5c5d04dc22d711485910e0c32510
Your Name <you@example.com>
2023-11-08 16:03:42 +0100

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    avatar: {
+        type: String,
+        default: "https://th.bing.com/th/id/OIP.Ghae4OEdb4UmC3hkqpFvLAHaGd?pid=ImgDet&rs=1"
+    },
 }, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-real-estate-app-4c983.firebaseapp.com",
+  projectId: "mern-real-estate-app-4c983",
+  storageBucket: "mern-real-estate-app-4c983.appspot.com",
+  messagingSenderId: "296489681420",
+  appId: "1:296489681420:web:d685e664c31e84fb29e429",
+  measurementId: "G-FQW9NJ5P3V"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
